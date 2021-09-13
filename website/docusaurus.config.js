e24d173cc388ad05e5a16ef11efdf8fe15a2a78e
huutrinh68 <trinhsp89@gmail.com>
2021-09-13 20:54:53 +0900

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,13 +23,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/sfbsoft/techblog/website/',
+          editUrl: 'https://github.com/sfbsoft/techblog/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-          'https://github.com/sfbsoft/techblog/website/blog/',
+          editUrl: 'https://github.com/sfbsoft/techblog/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
